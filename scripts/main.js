9d50842fd891fa68eebbd95e06d6f392414e88b7
zhangzhonghao <843202316@qq.com>
2023-06-06 09:39:41 +0800

    init

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+let head1 = document.querySelector('h1');
+head1.textContent = 'hello world';
+let num1 = 123;
+num1 = fun1()
+function fun1() {
+    return 1111;
+}
+document.querySelector('#p_end').addEventListener('click', function (){
+    alert('hello world!')
+})
